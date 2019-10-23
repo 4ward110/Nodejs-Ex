936929292ac329bb17c95ff5f51bb8e884ea6fd1
long.vu <vuduclong10a1cma@gmail.com>
2019-10-23 23:09:09 +0700

    modified index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/',(req,res) => {
 
 app.use('/users',authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRoute);
-app.use('/products', productRoute);
+app.use('/products',authMiddleware.requireAuth, productRoute);
 
 app.listen(port, () => {
     console.log('success!!!');
